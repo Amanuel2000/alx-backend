adebb5faea44bdd890faff84765e5a4f1179fcb4
amanuel2000 <amanuelf44@gmail.com>
2023-05-17 07:10:41 +0000

    basic cach

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
-""" 
-A class called BasicCache that inherits from BaseCaching
+#!/usr/bin/env python3
+"""Create a class BasicCache that inherits from
+BaseCaching and is a caching system:
 """
 
 
@@ -8,28 +8,32 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """
-        defineing class
+    """_summary__
     """
 
     def __init__(self):
-        """
-        super vlaue
+        """_summary_
         """
         super().__init__()
-    
+
     def put(self, key, item):
+        """_summary_
 
-
+        Args:
+                key (_type_): _description_
+                item (_type_): _description_
+        """
         if key is None or item is None:
             pass
         else:
             self.cache_data[key] = item
-    
+
     def get(self, key):
+        """return the value in self.cache_data linked to key
 
-        """return the value of the data"""
-
+        Args:
+                key (_type_): _description_
+        """
         if key is None or key not in self.cache_data.keys():
             return None
         return self.cache_data.get(key)
